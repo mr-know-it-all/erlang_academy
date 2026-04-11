0ac41d8ec1f04a84b9e2f0feabbad81414baaaff
paul <>
2026-04-11 17:05:00 +0300

    feat: week 4 homework

---
+++ Week #4/Homework 1/restarter/counter.erl
@@ -0,0 +1,14 @@
+-module(counter).
+-export([loop/1]).
+
+%% loop/1 - Simple counter that logs with timestamp at regular intervals
+loop(State) ->
+    {H, M, S} = erlang:time(),
+    io:format("counter [~2..0w:~2..0w:~2..0w]: state=~p~n", [H, M, S, State]),
+    if State >= 4 ->
+        io:format("counter: Crashing at state=~p!~n", [State]),
+        erlang:error("Counter reached 4, intentional crash!");
+    true ->
+        timer:sleep(500),
+        loop(State + 1)
+    end.
